6fff4cf672a7577f7a3a519c3891c0709335d3a2
Sopr-Agil <34137553+Sopr-Agil@users.noreply.github.com>
2018-06-01 16:29:18 -0500

    imagenes portal

---
+++ SQL.sql
@@ -0,0 +1,6 @@
+INSERT INTO tusuarios
+(idusuario,username,nombre,apaterno,amaterno,email,password,enable,cidperfil)
+VALUES (0,'admon','admon','','','vgcruz@','vgcruz@',1,0);
+
+
+SELECT * FROM tusuarios LIMIT 10000;
