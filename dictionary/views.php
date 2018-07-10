87731ded18162ca48a958237c05e77ccf2289da2
Sopr-Agil <34137553+Sopr-Agil@users.noreply.github.com>
2018-07-10 17:33:44 -0500

    imagenes portal

--- dictionary/views.php
+++ dictionary/views.php
@@ -3,4 +3,8 @@
     const VIEW_DEFAULT = 'default';  
     const VIEW_USERS ='alta-usuario';
     const VIEW_MAIN = 'main';
+    
+    #RENDERED
+    const TITULO = '<!--#title%-->';
+    const FORM_LOGIN = '<!--#login%-->';
 ?>    
