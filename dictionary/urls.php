b4d86b8016ff9517bca20353c8cf35b081489a94
Sopr-Agil <34137553+Sopr-Agil@users.noreply.github.com>
2018-07-10 17:29:03 -0500

    imagenes portal

--- dictionary/urls.php
+++ dictionary/urls.php
@@ -1,8 +1,7 @@
 <?php
     const ROOT ='CMITEMM';
-    const HOME ='es';
-    const PATH_TEMPLATE ='assets/html/templates/template-';
-    const TEMPLATE ='../assets/html/templates/template-';
-    const PATH_FORM ='assets/html/forms/form-';
-    const PATH_IMG ='assets/img/';    
+    const HOME ='es';    
+    const PATH_TEMPLATE ='../assets/html/templates/template-';
+    const PATH_FORM ='../assets/html/forms/form-';
+    const PATH_IMG ='../assets/img/';    
 ?>
