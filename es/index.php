cfb87e9800755f8c0173392801b2c5353c3bd63d
Sopr-Agil <34137553+Sopr-Agil@users.noreply.github.com>
2018-07-10 16:19:25 -0500

    imagenes portal

--- es/index.php
+++ es/index.php
@@ -1,15 +1,13 @@
 <?php 
-
-require_once  '../dictionary/urls.php';
+require_once '../dictionary/urls.php';
 require_once '../core/bo/Log.php';
-
+require_once '../core/bo/HtmlBo.php';
+require 'control.php';
 
 $str_url = $_SERVER['REQUEST_URI'];
 $metod = str_replace(ROOT, '', $str_url);
 $metod = str_replace(HOME, '', $metod);
 $metod = str_replace('/', '', $metod);
-
-echo $metod;
-//handler($metod);
+handler($metod);
 
 ?>
