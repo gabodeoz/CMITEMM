6e99060c1ee96d50cabeefee43af30225c727cda
Sopr-Agil <34137553+Sopr-Agil@users.noreply.github.com>
2018-07-25 12:32:14 -0500

    imagenes portal

--- core/dao/ConnectionDB.php
+++ core/dao/ConnectionDB.php
@@ -42,63 +42,78 @@ abstract class ConnectionDB {
             }
         }
     # Ejecutar un query simple del tipo INSERT, DELETE, UPDATE
-        protected function exec_query($array = array()) {
-            try {
-                $this->open_connection();
-                $sth = $this->conn->prepare($this->query);
-                if (count($array) > 0) {
-                    $sth->execute($array);
-                } else {
-                    $sth->execute();
-                }
-                $this->response=$sth->rowCount();
-            } catch (Exception $e) {
-                Log::write($e->getMessage());                
-            }
-            $sth = null;
-            $this->conn = null;
-        }
-
-    # Traer resultados de una consulta en un Array
-        protected function get_results_from_query($array= array()) {           
-           try{
-                $this->open_connection();
-                $sth = $this->conn->prepare($this->query);
-                    if(count($array)>0)
-                       $sth->execute($array);
-                    else
-                       $sth->execute();                 
-
-                while ($this->rows[]= $sth->fetch(PDO::FETCH_ASSOC));
-                    array_pop($this->rows);                    
-            }catch (Exception $e){
-                Log::write ( $e->getMessage());
-            }                       
-            $sth= null;
-            $this->conn=null;
-        }
-    #get object list
-    protected function get_result_objects($array = array()) {            
-            $i=0;            
+    protected function exec_query($array = array()) {
+        try {
             $this->open_connection();
             $sth = $this->conn->prepare($this->query);
-                    if(count($array)>0)
-                       $sth->execute($array);
-                    else
-                       $sth->execute();                 
-                                                                
-            while ($this->rows[]= $sth->fetch(PDO::FETCH_ASSOC)){
-                $nestedData=array();
-               foreach($this->rows[$i] as $key=>$value){
-                  $nestedData[] =$this->rows[$i][$key];
-               }//fin de foreach 
-               $this->data[] = $nestedData;
-               $i++;
-            }//fin de while                       
+            if (count($array) > 0) {
+                $sth->execute($array);
+            } else {
+                $sth->execute();
+            }
+            $this->response = $sth->rowCount();
+        } catch (Exception $e) {
+            Log::write($e->getMessage());
+        }
+        $sth = null;
+        $this->conn = null;
+    }
+
+    # Traer resultados de una consulta en un Array
+
+    protected function get_results_from_query($array = array()) {
+        try {
+            $this->open_connection();
+            $sth = $this->conn->prepare($this->query);
+            if (count($array) > 0)
+                $sth->execute($array);
+            else
+                $sth->execute();
+
+            while ($this->rows[] = $sth->fetch(PDO::FETCH_ASSOC));
             array_pop($this->rows);
-            $sth= null;
-            $this->conn=null;
-	}    
+        } catch (Exception $e) {
+            Log::write($e->getMessage());
+        }
+        $sth = null;
+        $this->conn = null;
+    }
+
+    #get object list
+
+    protected function get_result_objects($array = array()) {
+        $i = 0;
+        $this->open_connection();
+        $sth = $this->conn->prepare($this->query);
+        if (count($array) > 0)
+            $sth->execute($array);
+        else
+            $sth->execute();
+
+        while ($this->rows[] = $sth->fetch(PDO::FETCH_ASSOC)) {
+            $nestedData = array();
+            foreach ($this->rows[$i] as $key => $value) {
+                $nestedData[] = $this->rows[$i][$key];
+            }//fin de foreach 
+            $this->data[] = $nestedData;
+            $i++;
+        }//fin de while                       
+        array_pop($this->rows);
+        $sth = null;
+        $this->conn = null;
+    }
+
+    #get text sql        
+
+    protected function getSql($sqlFile) {
+        $str_sql = '';
+        $file_path = PATH_SQL . $sqlFile . '.sql';
+        if (is_readable($file_path)) {
+            $str_sql = file_get_contents($file_path);
+        }
+        $this->query = $str_sql;
+    }
+
     #get string connection    
         private function getStrConection($engine){            
             if($engine=='sqlsrv')
@@ -107,15 +122,7 @@ abstract class ConnectionDB {
                 return $engine.":host=". $this->server .";dbname=". $this->dbname ;
                 
         }
-    #get text sql        
-        protected function getSql($sqlFile) {
-            $str_sql = '';
-            $file_path = PATH_SQL . $sqlFile . '.sql';
-            if (is_readable($file_path)) {
-                 $str_sql = file_get_contents($file_path);
-            } 
-            $this->query = $str_sql;
-        }
+    
     # Método destructor del objeto
         function __destruct() {
             unset($this);
