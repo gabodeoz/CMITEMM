ae6b68a7933a251decc21affade73c67bd84423a
Sopr-Agil <34137553+Sopr-Agil@users.noreply.github.com>
2018-07-25 14:16:00 -0500

    imagenes portal

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,22 @@
-
-
-
+$(document).ready(function () {
+    $("#login").submit(function () {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: $(this).attr('action'),
+            data: $(this).serialize(),
+            beforeSend: function () {
+                $("#loading").show();
+            },
+            success: function (value) {
+                if (value.response == 1) {
+                    window.location = '%VIEW_HOME%';
+                    console.log(value.response);
+                } else
+                    $.notify("Usuario ó contraseña no validos", "warn");
+                $("#loading").hide();
+            }
+        });
+        return false;
+    });
+});      
